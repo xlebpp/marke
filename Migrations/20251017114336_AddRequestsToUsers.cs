d5db782cbd3090efc0b25150e010a8e6a400e613
dasha <daria.zvdsk@gmail.com>
2025-10-17 14:52:36 +0300

    migrations add requests into users

---
+++ Migrations/20251017114336_AddRequestsToUsers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace marketplaceE.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRequestsToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
